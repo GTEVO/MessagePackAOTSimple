ac94c6e400f96c485f6977e7ad8d513ac443dbeb
gt <gt@gt.com>
2020-01-10 19:28:00 +0800

    新测试

---
+++ MessagePackAOT/Assets/Scripts/AsyncUDP.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+
+public class AsyncUDP : MonoBehaviour
+{
+    byte[] _buffer;
+    EndPoint _remoteEP;
+    CancellationTokenSource _cancellationTokenSource;
+
+    Socket _socket;
+
+    public void OnClickStart()
+    {
+        _buffer = new byte[ushort.MaxValue];
+        _remoteEP = new IPEndPoint(IPAddress.Any, IPEndPoint.MinPort);
+        _cancellationTokenSource = new CancellationTokenSource();
+
+        var ip = new IPEndPoint(IPAddress.Any, 8000);
+        _socket = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+        _socket.Bind(ip);
+        var recvTask = new Task(async () => {
+            while (!_cancellationTokenSource.IsCancellationRequested) {
+                var result = await Task.Factory.FromAsync(BeginRecvFrom, EndRecvFrom, _socket, TaskCreationOptions.AttachedToParent);
+                Debug.Log(result);
+            }
+        }, _cancellationTokenSource.Token, TaskCreationOptions.LongRunning);
+        recvTask.ContinueWith(task => {
+            Debug.LogError(task.Exception);
+        }, TaskContinuationOptions.OnlyOnFaulted).Start();
+    }
+
+    private IAsyncResult BeginRecvFrom(AsyncCallback callback, object state)
+    {
+        var result = _socket.BeginReceiveFrom(_buffer, 0, ushort.MaxValue, SocketFlags.None, ref _remoteEP, callback, state);
+        return result;
+    }
+
+    private IAsyncResult EndRecvFrom(IAsyncResult result)
+    {
+        //  var recvBytes = _socket.EndReceiveFrom(result, ref _remoteEP);
+        return result;
+    }
+
+    public void OnClickStop()
+    {
+        _cancellationTokenSource.Cancel();
+        OnDestroy();
+    }
+
+    private void OnDestroy()
+    {
+        if (_socket != null) {
+            _socket.Close();
+            _socket.Dispose();
+        }
+    }
+}
---
+++ MessagePackAOT/Assets/Scripts/JsonTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+namespace JsonItem2
+{
+    public struct StructItem
+    {
+        public int Level { get; set; }
+        public int Exp { get; set; }
+        public int DeltaExp { get; set; }
+    }
+
+    public class ItemsArray
+    {
+        public StructItem[] Items { get; set; }
+    }
+}
+
+public class JsonTest : MonoBehaviour
+{
+    [SerializeField] Text _text;
+
+    private void Start()
+    {
+        InjectMod();
+    }
+
+
+    public void Test()
+    {
+        var json = string.Format("{{\"Level\":{0},\"Exp\":12,\"DeltaExp\":2}}", (int)(Time.time * 1000));
+        var obj2 = JsonConvert.DeserializeObject<JsonItem.StructItem>(json);
+        _text.text = obj2.Level.ToString();
+    }
+
+    public void Test2()
+    {
+        var json = string.Format("{{\"Items\":[{{\"Level\":{0},\"Exp\":12,\"DeltaExp\":2}}]}}", (int)(Time.time * 1000));
+        var obj2 = JsonConvert.DeserializeObject<JsonItem.ItemsArray>(json);
+        _text.text = obj2.Items[0].Level.ToString();
+    }
+
+    public void Test3()
+    {
+        var json = string.Format("{{\"Items\":[{{\"Level\":{0},\"Exp\":12,\"DeltaExp\":2}}]}}", (int)(Time.time * 1000));
+        var obj2 = JsonConvert.DeserializeObject<JsonItem2.ItemsArray>(json);
+        _text.text = obj2.Items[0].Level.ToString();
+    }
+
+    private void InjectMod()
+    {
+        var json = string.Format("{{\"Items\":[{{\"Level\":{0},\"Exp\":12,\"DeltaExp\":2}}]}}", (int)(Time.time * 1000));
+        var obj2 = JsonConvert.DeserializeObject<JsonItem2.ItemsArray>(json);
+        _text.text = obj2.Items[0].Level.ToString();
+    }
+
+    /*
+public void Test4()
+{
+    var json = string.Format("{{\"Level\":{0},\"Exp\":12,\"DeltaExp\":2}}", (int)(Time.time * 1000));
+    var obj2 = JsonConvert.DeserializeObject<Vector2>(json);
+    _text.text = obj2.Level.ToString();
+}*/
+}
