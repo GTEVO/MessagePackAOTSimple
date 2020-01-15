18b50800b126e1372d094b4c14d9b02d6c1c1bf4
gt <gteavarro@gmail.com>
2020-01-16 01:32:07 +0800

    task 调度 测试

--- MessagePackAOT/Assets/Scripts/MsgPackTest.cs
+++ MessagePackAOT/Assets/Scripts/MsgPackTest.cs
@@ -14,6 +14,7 @@ using Debug = CommonLib.Debug;
 using MsgDefine.TestMsg;
 using MsgDefine.Resolvers;
 using ClientLib;
+using System.Net.Http;
 
 public class MsgPackTest : MonoBehaviour
 {
@@ -81,7 +82,8 @@ public class MsgPackTest : MonoBehaviour
 
     public void OnClickDeserialize()
     {
-        var loginMsg = new LoginReqMsg {
+        var loginMsg = new LoginReqMsg
+        {
             Account = "account",
             Password = "pwd",
             Extra = "哈哈哈",
@@ -89,7 +91,8 @@ public class MsgPackTest : MonoBehaviour
         var reqMsgBytes = MessagePackSerializer.Serialize(loginMsg);
         var reqMsgreqMsgObj = MessagePackSerializer.Deserialize<LoginReqMsg>(reqMsgBytes);
 
-        var registerMsg = new RegisterReqMsg {
+        var registerMsg = new RegisterReqMsg
+        {
             Phone = "1350000",
             Authcode = "验证码",
         };
@@ -101,18 +104,60 @@ public class MsgPackTest : MonoBehaviour
         fs.Dispose();
 
         var a = new CancellationTokenSource();
-        Task.Factory.StartNew(() => {
-            try {
-                var registerMsgBytes = MessagePackSerializer.Serialize(registerMsg);
-                var deserializeMsg = MessagePackSerializer.Deserialize<RegisterReqMsg>(registerMsgBytes);
-                text.text = string.Format("Account : {0}-{1}-{2}", reqMsgreqMsgObj.Account
-                    , reqMsgreqMsgObj.Password, reqMsgreqMsgObj.Extra);
+        Debug.LogFormat("unity main {0}", Thread.CurrentThread.ManagedThreadId);
 
-                text2.text = string.Format("Account : {0}-{1}", deserializeMsg.Phone, deserializeMsg.Authcode);
-            }
-            catch {
-            }
-        }, _cancellationTokenSource.Token, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext()).Wait();
+
+        var mainThreadTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        Task.Factory.StartNew(async () =>
+         {
+             try
+             {
+                 Debug.LogFormat("before await {0}", Thread.CurrentThread.ManagedThreadId);
+                 await Wait();
+                 Debug.LogFormat("after await {0}", Thread.CurrentThread.ManagedThreadId);
+                 var registerMsgBytes = MessagePackSerializer.Serialize(registerMsg);
+                 var deserializeMsg = MessagePackSerializer.Deserialize<RegisterReqMsg>(registerMsgBytes);
+                 text.text = string.Format("Account : {0}-{1}-{2}", reqMsgreqMsgObj.Account
+                     , reqMsgreqMsgObj.Password, reqMsgreqMsgObj.Extra);
+                 text2.text = string.Format("Account : {0}-{1}", deserializeMsg.Phone, deserializeMsg.Authcode);
+             }
+             catch (System.Exception e)
+             {
+                 UnityEngine.Debug.LogError(e);
+             }
+             return true;
+         }, _cancellationTokenSource.Token, TaskCreationOptions.None
+           , mainThreadTaskScheduler);
+    }
+
+    private async Task RequstHttp()
+    {
+        Debug.LogFormat("work task start {0}", Thread.CurrentThread.ManagedThreadId);
+        await Task.Delay(1000);
+        HttpClient http = new HttpClient();
+        var txt = await http.GetStringAsync("http://www.baidu.com");
+        UnityEngine.Debug.Log(txt);
+        Debug.LogFormat("work task stop {0}", Thread.CurrentThread.ManagedThreadId);
+    }
+
+    private async Task Wait()
+    {
+        Debug.LogFormat("async task A {0}", Thread.CurrentThread.ManagedThreadId);
+        /// await RequstHttp();
+        await Task.Run(RequstHttp);
+        Debug.LogFormat("async task B {0}", Thread.CurrentThread.ManagedThreadId);
+        await Task.Delay(1 * 1000);
+        Debug.LogFormat("async task C {0}", Thread.CurrentThread.ManagedThreadId);
+        await Task.Delay(1 * 1000);
+        Debug.LogFormat("async task D {0}", Thread.CurrentThread.ManagedThreadId);
+
+        //  task 调用方式
+        //  测试表明：
+        //  1、对于使用new创建的task，await关键字不能让task被调度，需要显式调用Start()，调度器默认使用 TaskScheduler.Current
+        //  2、Task.RunTask、Task.Factory.StartNew创建的task，调度器默认使用 TaskScheduler.Default
+        //  3、默认情况下，TaskScheduler.Current = TaskScheduler.Default
+        //  4、异步方法的调用，同new创建的task一样，并且也遵循 2、3
+        //  5、异步task中使用 await 一个 task之后，当前调度会变成这个task的调度器
     }
 
     public void OnClickOnHandleMsg()
@@ -136,10 +181,11 @@ public class MsgPackTest : MonoBehaviour
         //
         sending = true;
         StartCoroutine(SendMsg());
-       
+
     }
 
-    public void ClickCancelSend() {
+    public void ClickCancelSend()
+    {
         sending = false;
     }
 
@@ -147,12 +193,14 @@ public class MsgPackTest : MonoBehaviour
 
     private IEnumerator SendMsg()
     {
-        var loginReq = new LoginReqMsg {
+        var loginReq = new LoginReqMsg
+        {
             Account = "A",
             Password = "PWD",
             Extra = "额外"
         };
-        do {
+        do
+        {
             App.Instacne.UdpClient.SendMessage(loginReq);
             yield return null;
         } while (sending);
