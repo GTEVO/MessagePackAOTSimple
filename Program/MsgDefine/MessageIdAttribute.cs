2e0812685b8f11df1cd1dea1cb97be736efd04f7
gt <gt@gt.com>
2020-01-22 18:02:52 +0800

    删除没用到的定义

---
+++ Program/MsgDefine/MessageIdAttribute.cs
@@ -0,0 +1,18 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MsgDefine
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+    public class MessageIdAttribute : Attribute
+    {
+        public int Id { get; private set; }
+
+        public MessageIdAttribute(int id)
+        {
+            Id = id;
+        }
+    }
+}
